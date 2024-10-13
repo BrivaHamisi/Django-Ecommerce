4203aad1585b0c67f620311d1c7102183d4cda2a
BrivaHamisi <brivahamisi@gmail.com>
2024-10-13 20:14:05 +0300

    STABLE: Stable Release

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,6 +125,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_ROOT =os.path.join(BASE_DIR, 'static/images')
+
+MEDIA_URL = '/images/products/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
