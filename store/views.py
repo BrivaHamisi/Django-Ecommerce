43a96e319c74eccd4b38a304c347c517a90667d3
BrivaHamisi <brivahamisi@gmail.com>
2024-10-14 21:22:17 +0300

    Store, Checkout Updated with data

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Product, Order
 
 
 # Create your views here.
@@ -12,11 +12,27 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = { 'get_cart_total':0, 'get_cart_items':0 }
 
+    context = {
+        'items': items, 'order': order
     }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        items = []
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
